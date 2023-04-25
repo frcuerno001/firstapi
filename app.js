94fd8a45686036702b7d5ddada376fc66e33d13a
frcuerno@gbm.net <frcuerno@gbm.net>
2023-04-25 10:48:01 -0600

    se agrego cors auth

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 
+app.use(cors({
+    origin: '*'
+}));
+
+const location = [
+    {
+        "id": "1",
+        "item": "Millenium"
+    }
+];
+
 app.get('/',(req,res) => {
     res.send("Welcome to first app Cuerno")
 });
+
+app.get('/location',(req,res) => {
+    res.send(location)
+});
+
 
 app.listen(4000, ()=>{
     console.log("Listening to port 4000");
