59bf03014d3578c0f1d63bf6febcb0b1f0957096
frcuerno@gbm.net <frcuerno@gbm.net>
2023-04-25 11:08:06 -0600

    cambios a nivel de cors auth

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var cors = require('cors')
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 
-const location = [
+var location = [
     {
         "id": "1",
         "item": "Millenium"
