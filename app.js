df6df49cac02d3bdb984407d4c78638536667799
Francisco Cuerno <frcuerno001@gmail.com>
2023-05-14 20:34:46 -0600

    cambios en cors

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+
 app.post('/location', (req, res) => {
     console.log(res.body);
     location.push(res.body);
@@ -25,7 +31,7 @@ app.get('/location',(req,res) => {
 });
 
 //Metodo para extraer GET
-app.get('/places', async(req,res) => {
+app.get('/places', cors(corsOptions), async(req,res) => {
     try {
         const places = await Places.find({});
         res.status(200).json(places);
@@ -36,7 +42,7 @@ app.get('/places', async(req,res) => {
 
 
 //Metodo para extraer GET por ID
-app.get('/places/:id', async(req,res) => {
+app.get('/places/:id', cors(corsOptions), async(req,res) => {
     try {
         const {id} = req.params;
         const places = await Places.findById(id);
@@ -49,7 +55,7 @@ app.get('/places/:id', async(req,res) => {
 
 
 //Actualizar Productos
-app.put('/places/:id', async(req,res) => {
+app.put('/places/:id', cors(corsOptions), async(req,res) => {
     try {
         const {id} = req.params;
         const places = await Places.findByIdAndUpdate(id, req.body);
@@ -67,7 +73,7 @@ app.put('/places/:id', async(req,res) => {
 
 
 //Eliminar producto
-app.delete('/places/:id', async(req, res) =>{
+app.delete('/places/:id', cors(corsOptions), async(req, res) =>{
     try {
         const {id} = req.params;
         const places = await Places.findByIdAndDelete(id); 
@@ -82,7 +88,7 @@ app.delete('/places/:id', async(req, res) =>{
 
 
 //Metodo para enviar datos POST
-app.post('/places', async(req, res) => {
+app.post('/places', cors(corsOptions), async(req, res) => {
     try {
         const places = await Places.create(req.body);
         res.status(200).json(places);         
@@ -93,7 +99,7 @@ app.post('/places', async(req, res) => {
 });
 
 //Actualizar Productos
-app.put('/places/:id', async(req,res) => {
+app.put('/places/:id', cors(corsOptions), async(req,res) => {
     try {
         const {id} = req.params;
         const places = await Places.findByIdAndUpdate(id, req.body);
